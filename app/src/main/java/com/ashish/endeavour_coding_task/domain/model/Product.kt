244b4201865679fff5d71dde36fd57bb677c065b
ashish <ashish.budhathoki@outlook.com>
2022-10-06 19:28:28 +1100

    remove price message field

--- app/src/main/java/com/ashish/endeavour_coding_task/domain/model/Product.kt
+++ app/src/main/java/com/ashish/endeavour_coding_task/domain/model/Product.kt
@@ -5,7 +5,6 @@ data class Product(
     val imageUrl: String,
     val name: String,
     val price: Double,
-    val priceMessage: String,
     val isFavourite: Boolean,
     val rating: Double,
 )
