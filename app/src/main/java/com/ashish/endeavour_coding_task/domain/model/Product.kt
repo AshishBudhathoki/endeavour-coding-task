c29630aef24e0d03d7ef8780bae93d4f426ec171
ashish <ashish.budhathoki@outlook.com>
2022-10-06 17:10:24 +1100

    create domain model

---
+++ app/src/main/java/com/ashish/endeavour_coding_task/domain/model/Product.kt
@@ -0,0 +1,11 @@
+package com.ashish.endeavour_coding_task.domain.model
+
+data class Product(
+    val id: Long,
+    val imageUrl: String,
+    val name: String,
+    val price: Double,
+    val priceMessage: String,
+    val isFavourite: Boolean,
+    val rating: Double,
+)
