8fa1d988038c634260dd41ca96a7c9351fe62036
ashish <ashish.budhathoki@outlook.com>
2022-10-06 19:11:08 +1100

    create entity

---
+++ app/src/main/java/com/ashish/endeavour_coding_task/data/local/entity/ProductEntity.kt
@@ -0,0 +1,16 @@
+package com.ashish.endeavour_coding_task.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class ProductEntity(
+    @PrimaryKey
+    val id: String,
+    val imageUrl: String,
+    val name: String,
+    val price: Double,
+    val priceMessage: String,
+    val isFavourite: Boolean,
+    val rating: Double,
+)
