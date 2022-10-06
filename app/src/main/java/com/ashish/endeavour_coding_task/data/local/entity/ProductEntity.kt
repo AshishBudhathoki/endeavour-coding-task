5c985e9f7daa281ce536e91fbbb0a183bf4d25cc
ashish <ashish.budhathoki@outlook.com>
2022-10-06 19:33:50 +1100

    remove price message from entity

--- app/src/main/java/com/ashish/endeavour_coding_task/data/local/entity/ProductEntity.kt
+++ app/src/main/java/com/ashish/endeavour_coding_task/data/local/entity/ProductEntity.kt
@@ -10,7 +10,6 @@ data class ProductEntity(
     val imageUrl: String,
     val name: String,
     val price: Double,
-    val priceMessage: String,
     val isFavourite: Boolean,
     val rating: Double,
 )
