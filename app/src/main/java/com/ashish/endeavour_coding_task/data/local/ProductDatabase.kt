3d58c0817eb1b4adbbfdd00e602cbc9a42f368d1
ashish <ashish.budhathoki@outlook.com>
2022-10-06 19:21:08 +1100

    add missing room database extension

--- app/src/main/java/com/ashish/endeavour_coding_task/data/local/ProductDatabase.kt
+++ app/src/main/java/com/ashish/endeavour_coding_task/data/local/ProductDatabase.kt
@@ -1,12 +1,13 @@
 package com.ashish.endeavour_coding_task.data.local
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import com.ashish.endeavour_coding_task.data.local.entity.ProductEntity
 
 @Database(
     entities = [ProductEntity::class],
     version = 1
 )
-abstract class ProductDatabase {
+abstract class ProductDatabase : RoomDatabase() {
     abstract val dao: ProductDao
 }
