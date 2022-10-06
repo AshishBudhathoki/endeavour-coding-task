98ec2cf436959baddb53f0c8ac8686a36d3488cf
ashish <ashish.budhathoki@outlook.com>
2022-10-06 19:22:06 +1100

    create retrofit and room dependencies

---
+++ app/src/main/java/com/ashish/endeavour_coding_task/data/di/ProductDataModule.kt
@@ -0,0 +1,54 @@
+package com.ashish.endeavour_coding_task.data.di
+
+import android.app.Application
+import androidx.room.Room
+import com.ashish.endeavour_coding_task.data.local.ProductDatabase
+import com.ashish.endeavour_coding_task.data.remote.ProductApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ProductDataModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(
+                HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BODY
+                }
+            )
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideProductApi(client: OkHttpClient): ProductApi {
+        return Retrofit.Builder()
+            .baseUrl(ProductApi.BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .client(client)
+            .build()
+            .create()
+    }
+
+    @Provides
+    @Singleton
+    fun provideProductDatabase(app: Application): ProductDatabase {
+        return Room.databaseBuilder(
+            app,
+            ProductDatabase::class.java,
+            "product.db"
+        ).build()
+    }
+}
